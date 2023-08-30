9d37acc6eeb4c6a29471e7d0f356df7ac3f81555
yonatanBtl <yonatan.bartolo@unmsm.edu.pe>
2023-08-30 13:28:58 -0500

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "19200166_20200031_HappyBirthday"
+include(":app")
